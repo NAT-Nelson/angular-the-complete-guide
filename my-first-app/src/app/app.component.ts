ea5d9af9a1f02e30b8f10bcbc64af8319f82c65e
Jason Nelson <jmnelson613@gmail.com>
2025-01-27 10:09:11 -0700

    23 - Working with Component Styles COMPLETED

--- my-first-app/src/app/app.component.ts
+++ my-first-app/src/app/app.component.ts
@@ -4,7 +4,12 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   templateUrl: './app.component.html',
   standalone: false,
-  styleUrl: './app.component.css'
+  // styleUrl: './app.component.css'
+  styles: [`
+    h3 {
+      color: dodgerblue;
+    }
+  `]
 })
 export class AppComponent {
 }
