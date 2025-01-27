06953314b53db8e7685b3049bd0798cbfbdc8974
Jason Nelson <jmnelson613@gmail.com>
2025-01-27 10:18:30 -0700

    24 - Fully Understanding the Component Selector COMPLETED

--- my-first-app/src/app/servers/servers.component.ts
+++ my-first-app/src/app/servers/servers.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-servers',
+  // selector: '[app-servers]',
+  selector: '.app-servers',
   standalone: false,
   
   template: `
