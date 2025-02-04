dccb9d9229e38fcab7dfb5d22216bbca96a70f45
Jason Nelson <jmnelson613@gmail.com>
2025-02-04 07:14:41 -0700

    56 - Adding Content to the Recipes Components COMPLETED

--- ng4-the-project/src/app/recipes/recipe-list/recipe-list.component.ts
+++ ng4-the-project/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import { Recipe } from './../recipe.model';
+
 @Component({
   selector: 'app-recipe-list',
   standalone: false,
@@ -8,6 +10,10 @@ import { Component } from '@angular/core';
   styleUrl: './recipe-list.component.css'
 })
 export class RecipeListComponent {
-  recipes = [];
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is simply a test', 
+      'https://www.add1tbsp.com/wp-content/' +
+      'uploads/2021/06/20210402-MLMC-Split-Lobster-Herb-Butter1444-scaled-1366x2048.jpg')
+  ];
 
 }
