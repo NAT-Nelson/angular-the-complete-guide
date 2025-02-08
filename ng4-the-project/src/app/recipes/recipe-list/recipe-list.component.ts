3a205e3c2caee6031c4b5350748d80f78854c04b
Jason Nelson <jmnelson613@gmail.com>
2025-02-08 09:31:39 -0700

    57 - Outputting a List of Recipes with ngFor COMPLETED

--- ng4-the-project/src/app/recipes/recipe-list/recipe-list.component.ts
+++ ng4-the-project/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -13,7 +13,10 @@ export class RecipeListComponent {
   recipes: Recipe[] = [
     new Recipe('A Test Recipe', 'This is simply a test', 
       'https://www.add1tbsp.com/wp-content/' +
-      'uploads/2021/06/20210402-MLMC-Split-Lobster-Herb-Butter1444-scaled-1366x2048.jpg')
+      'uploads/2021/06/20210402-MLMC-Split-Lobster-Herb-Butter1444-scaled-1366x2048.jpg'),
+      new Recipe('A Test Recipe', 'This is simply a test', 
+        'https://www.add1tbsp.com/wp-content/' +
+        'uploads/2021/06/20210402-MLMC-Split-Lobster-Herb-Butter1444-scaled-1366x2048.jpg')
   ];
 
 }
